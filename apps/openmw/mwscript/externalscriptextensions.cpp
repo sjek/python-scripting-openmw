1d309053a05dd9e423f17bca738ba8f8420a5a3d
Scott Howard <showard314@gmail.com>
2015-01-31 15:16:58 -0500

    install extensions to LIBDIR/games/openmw/

--- apps/openmw/mwscript/externalscriptextensions.cpp
+++ apps/openmw/mwscript/externalscriptextensions.cpp
@@ -10,6 +10,7 @@
 #include <components/interpreter/runtime.hpp>
 #include <components/interpreter/opcodes.hpp>
 #include <components/interpreter/context.hpp>
+#include <components/files/configurationmanager.hpp>
 
 #include "interpretercontext.hpp"
 #include "extensions.hpp"
@@ -35,21 +36,54 @@ namespace MWScript
                     {
                         MWScript::InterpreterContext pythonContext = static_cast<InterpreterContext&> (runtime.getContext());
                         MWScriptExtensions::context = &pythonContext;
-                        //Interpreter::Interpreter pythonInterpreter;
-                        //MWScript::installOpcodes(pythonInterpreter);
-                        //MWScriptExtensions::interpreter = &pythonInterpreter; //need to create one interpreter, not recreat each time
                         if(!MWScriptExtensions::opcodesInstalled)
                         {
                             MWScript::installOpcodes(MWScriptExtensions::interpreter);
                             Py_Initialize();
-                            PyRun_SimpleString("from openmw import *\n");
+                            boost::program_options::variables_map variables;
+
+                            boost::program_options::options_description desc("Allowed options");
+                            desc.add_options()
+                            ("data", boost::program_options::value<Files::PathContainer>()->default_value(Files::PathContainer(), "data")->multitoken()->composing())
+                            ("data-local", boost::program_options::value<std::string>()->default_value(""))
+                            ("extensions", boost::program_options::value<std::string>()->default_value(""));
+
+                            boost::program_options::notify(variables);
+
+                            Files::ConfigurationManager cfgManager;
+                            cfgManager.readConfiguration(variables, desc);
+
+                            Files::PathContainer dataDirs, dataLocal;
+                            if (!variables["data"].empty()) {
+                                dataDirs = Files::PathContainer(variables["data"].as<Files::PathContainer>());
+                            }
+
+                            std::string local = variables["data-local"].as<std::string>();
+                            if (!local.empty()) {
+                                dataLocal.push_back(Files::PathContainer::value_type(local));
+                            }
+
+                            cfgManager.processPaths (dataDirs);
+                            cfgManager.processPaths (dataLocal, true);
+
+                            if (!dataLocal.empty())
+                                dataDirs.insert (dataDirs.end(), dataLocal.begin(), dataLocal.end());
+
+                            Files::PathContainer extensionDirs = variables["extensions"].as<Files::PathContainer>();
+                            extensionDirs.push_back(dataDirs);
+                            //need to find directories and append to sys.path, make new .cfg variable for extensions
+                            // "lib/games/openmw" and look in data dirs for other scripts
+                            // see "opemw/plugins/mygui_.../plugin.cpp for cfgManager.readConfiguration
+
+                            PyRun_SimpleString("import sys; sys.path.append(\".\"); from openmw import *\n");
+                            //PyRun_SimpleString("from openmw import *\n");
                             MWScriptExtensions::opcodesInstalled=true;
+
                         }
                         //PyThreadState* newPythonInterpreter = Py_NewInterpreter();
                         FILE *file_1 = fopen(scriptname.c_str(),"r");
                         PyRun_SimpleFileEx(file_1,scriptname.c_str(),1);
                         //Py_EndInterpreter(newPythonInterpreter);
-                        //MWScriptExtensions::interpreter=NULL;
                         MWScriptExtensions::context = NULL;
                     }
                     else
